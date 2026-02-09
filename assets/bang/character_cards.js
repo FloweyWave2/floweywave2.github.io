507a8782f14ecebbab3b650d77625ddae12e3e33
Michael Šlechtický <greenflowey@gmail.com>
2026-02-09 19:24:00 +0100

    Update character_cards.js

--- assets/bang/character_cards.js
+++ assets/bang/character_cards.js
@@ -4,6 +4,7 @@ const character_cards = [
     name: "Agnes Walker",
     hp: 4,
     set: "ZHP",
+    type: "Postava",
     text: "Ve fázi dobírání karet si dobíráš o 1 kartu navíc za každé 4 odhalené karty Pravé poledne.",
     image: "images/bang/cards/characters/agnes_walker.png"
   },
@@ -12,6 +13,7 @@ const character_cards = [
     name: "Amelia Lockhart",
     hp: 4,
     set: "ZHP",
+    type: "Postava",
     text: "Na začátku hry umísti do své brašny kartu Letadlo. Na začátku tahu si dober kartu z Leteckého balíčku. Jednou za svůj tah můžeš Letadlo přesunout do brašny hráče polevici/popravici od něj.",
     image: "images/bang/cards/characters/amelia_lockhart.png"
   },
@@ -20,6 +22,7 @@ const character_cards = [
     name: "Andrea Flytrap",
     hp: 4,
     set: "ZHP",
+    type: "Postava",
     text: "Jednou ve svém tahu můžeš umístit útočnou kartu s hnědým či zeleným okrajem lícem dolů do brašny jako “Past”. Poté co ti jiný hráč odhodí/ukradne kartu, či tě zasáhne, můžeš odhalit a zahrát jednu svoji “Past” proti tomuto hráči.",
     image: "images/bang/cards/characters/andrea_flytrap.png"
   },
@@ -28,6 +31,7 @@ const character_cards = [
     name: "Angelina Tango",
     hp: 4,
     set: "ZHP",
+    type: "Postava",
     text: "Ve svém tahu můžeš až jednou za tah prohodit 2 karty na stole.",
     image: "images/bang/cards/characters/angelina_tango.png"
   },
@@ -36,6 +40,7 @@ const character_cards = [
     name: "Anita Angel",
     hp: 4,
     set: "ZHP",
+    type: "Postava",
     text: "Poté co protihráč zraní jiného hráče, označ útočníka za Psance. Tvůj dostřel je proti Psancům nekonečný a při jejich zásahu kartou BANG! si dobereš kartu. Na konci tvého tahu jsou všichni Psanci ospravedlněni.",
     image: "images/bang/cards/characters/anita_angel.png"
   },
@@ -44,6 +49,7 @@ const character_cards = [
     name: "Carla Shaw",
     hp: 4,
     set: "ZHP",
+    type: "Postava",
     text: "Jednou za tah si můžeš líznout 2 karty, pokud zahraješ 3 karty za sebou, tak aby vytvořily vzrůstající nebo klesající číselnou řadu. (Na symbolu čísla nezáleží).",
     image: "images/bang/cards/characters/carla_shaw.png"
   },
@@ -52,6 +58,7 @@ const character_cards = [
     name: "Carmina Pale",
     hp: 4,
     set: "ZHP",
+    type: "Postava",
     text: "Začínáš hru s kartami Mustang a Appaloosa na stole. Karty tvých Koní jsou immuní vůči kartám Pravého poledne a kterýkoliv hráč jež tě o nějakého Koně připraví se stane cílem efektu BANG!",
     image: "images/bang/cards/characters/carmina_pale.png"
   },
@@ -60,6 +67,7 @@ const character_cards = [
     name: "Lady Constance",
     hp: 3,
     set: "ZHP",
+    type: "Postava",
     text: "Pokud bys měl/a být vyřazen/a, namísto opuštění hry si vyměň postavu za Polomrtvou Lady. Tvůj limit karet v ruce je vždy 4.",
     image: "images/bang/cards/characters/lady_constance.png"
   },
@@ -68,6 +76,7 @@ const character_cards = [
     name: "Lisa Elend",
     hp: 4,
     set: "ZHP",
+    type: "Postava",
     text: "Kdykoliv zraníš jednoho nebo více hráčů, vyléči si 1 zranění. (Namísto toho 2 zranění, pokud tvůj útok hráče vyřadil.)",
     image: "images/bang/cards/characters/lisa_elend.png"
   },
