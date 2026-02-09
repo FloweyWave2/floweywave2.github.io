dc0980c7ba9740fd8849af830047450f5f13d7be
Michael Šlechtický <greenflowey@gmail.com>
2026-02-09 17:06:53 +0100

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,18 +1,20 @@
 const container = document.getElementById("character-cards");
 
-character_cards.forEach(card => {
-  const cardEl = document.createElement("div");
-  cardEl.className = "character-card";
+if (container) {
+  character_cards.forEach(card => {
+    const cardEl = document.createElement("div");
+    cardEl.className = "character-card";
 
-  cardEl.innerHTML = `
-    <img src="${card.image}" alt="${card.name}">
-    <h3>${card.name}</h3>
-    <div class="card-meta">
-      <span class="hp">❤️ ${card.hp}</span>
-      <span class="set">${card.set}</span>
-    </div>
-    <p class="card-text">${card.text}</p>
-  `;
+    cardEl.innerHTML = `
+      <img src="${card.image}" alt="${card.name}">
+      <h3>${card.name}</h3>
+      <div class="card-meta">
+        <span class="hp">❤️ ${card.hp}</span>
+        <span class="set">${card.set}</span>
+      </div>
+      <p class="card-text">${card.text}</p>
+    `;
 
-  container.appendChild(cardEl);
-});
+    container.appendChild(cardEl);
+  });
+}
