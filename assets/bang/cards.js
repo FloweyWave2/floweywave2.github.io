927ae4b342e6b6a850f62d40320bce816473d876
Michael Šlechtický <greenflowey@gmail.com>
2026-02-09 16:43:50 +0100

    Update cards.js

--- assets/bang/cards.js
+++ assets/bang/cards.js
@@ -1,1 +1,74 @@
-ff
+const character_cards = [
+  {
+    id: 1,
+    name: "Agnes Walker",
+    hp: 4,
+    set: "ZHP",
+    text: "Ve fázi dobírání karet si dobíráš o 1 kartu navíc za každé 4 odhalené karty Pravé poledne.",
+    image: "images/cards/infernal-whelp.png"
+  },
+  {
+    id: 2,
+    name: "Amelia Lockhart",
+    hp: 4,
+    set: "ZHP",
+    text: "Na začátku hry umísti do své brašny kartu Letadlo. Na začátku tahu si dober kartu z Leteckého balíčku. Jednou za svůj tah můžeš Letadlo přesunout do brašny hráče polevici/popravici od něj.",
+    image: "images/cards/azure-guardian.png"
+  },
+  {
+    id: 3,
+    name: "Andrea Flytrap",
+    hp: 4,
+    set: "ZHP",
+    text: "Jednou ve svém tahu můžeš umístit útočnou kartu s hnědým či zeleným okrajem lícem dolů do brašny jako “Past”. Poté co ti jiný hráč odhodí/ukradne kartu, či tě zasáhne, můžeš odhalit a zahrát jednu svoji “Past” proti tomuto hráči.",
+    image: "images/cards/azure-guardian.png"
+  },
+  {
+    id: 4,
+    name: "Angelina Tango",
+    hp: 4,
+    set: "ZHP",
+    text: "Ve svém tahu můžeš až jednou za tah prohodit 2 karty na stole.",
+    image: "images/cards/azure-guardian.png"
+  },
+  {
+    id: 5,
+    name: "Anita Angel",
+    hp: 4,
+    set: "ZHP",
+    text: "Poté co protihráč zraní jiného hráče, označ útočníka za Psance. Tvůj dostřel je proti Psancům nekonečný a při jejich zásahu kartou BANG! si dobereš kartu. Na konci tvého tahu jsou všichni Psanci ospravedlněni.",
+    image: "images/cards/azure-guardian.png"
+  },
+  {
+    id: 6,
+    name: "Carla Shaw",
+    hp: 4,
+    set: "ZHP",
+    text: "Jednou za tah si můžeš líznout 2 karty, pokud zahraješ 3 karty za sebou, tak aby vytvořily vzrůstající nebo klesající číselnou řadu. (Na symbolu čísla nezáleží).",
+    image: "images/cards/azure-guardian.png"
+  },
+  {
+    id: 7,
+    name: "Carmina Pale",
+    hp: 4,
+    set: "ZHP",
+    text: "Začínáš hru s kartami Mustang a Appaloosa na stole. Karty tvých Koní jsou immuní vůči kartám Pravého poledne a kterýkoliv hráč jež tě o nějakého Koně připraví se stane cílem efektu BANG!",
+    image: "images/cards/azure-guardian.png"
+  },
+  {
+    id: 8,
+    name: "Lady Constance",
+    hp: 3,
+    set: "ZHP",
+    text: "Pokud bys měl/a být vyřazen/a, namísto opuštění hry si vyměň postavu za Polomrtvou Lady. Tvůj limit karet v ruce je vždy 4.",
+    image: "images/cards/azure-guardian.png"
+  },
+  {
+    id: 9,
+    name: "Lisa Elend",
+    hp: 4,
+    set: "ZHP",
+    text: "Kdykoliv zraníš jednoho nebo více hráčů, vyléči si 1 zranění. (Namísto toho 2 zranění, pokud tvůj útok hráče vyřadil.)",
+    image: "images/cards/azure-guardian.png"
+  },
+];
