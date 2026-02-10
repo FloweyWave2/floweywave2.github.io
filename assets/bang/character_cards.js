ad12d7beafdf13d0695820936ecf5a42ca5b5ace
Michael Šlechtický <greenflowey@gmail.com>
2026-02-10 18:46:49 +0100

    Update character_cards.js

--- assets/bang/character_cards.js
+++ assets/bang/character_cards.js
@@ -3,7 +3,7 @@ const character_cards = [
     id: 1,
     name: "Agnes Walker",
     hp: 4,
-    set: "ZHP",
+    set: "Za Hranicí Prérie",
     type: "Postava",
     text: "Ve fázi dobírání karet si dobíráš o 1 kartu navíc za každé 4 odhalené karty Pravé poledne.",
     image: "images/bang/cards/characters/agnes_walker.png"
@@ -12,7 +12,7 @@ const character_cards = [
     id: 2,
     name: "Amelia Lockhart",
     hp: 4,
-    set: "ZHP",
+    set: "Za Hranicí Prérie",
     type: "Postava",
     text: "Na začátku hry umísti do své brašny kartu Letadlo. Na začátku tahu si dober kartu z Leteckého balíčku. Jednou za svůj tah můžeš Letadlo přesunout do brašny hráče polevici/popravici od něj.",
     image: "images/bang/cards/characters/amelia_lockhart.png"
@@ -21,7 +21,7 @@ const character_cards = [
     id: 3,
     name: "Andrea Flytrap",
     hp: 4,
-    set: "ZHP",
+    set: "Za Hranicí Prérie",
     type: "Postava",
     text: "Jednou ve svém tahu můžeš umístit útočnou kartu s hnědým či zeleným okrajem lícem dolů do brašny jako “Past”. Poté co ti jiný hráč odhodí/ukradne kartu, či tě zasáhne, můžeš odhalit a zahrát jednu svoji “Past” proti tomuto hráči.",
     image: "images/bang/cards/characters/andrea_flytrap.png"
@@ -30,7 +30,7 @@ const character_cards = [
     id: 4,
     name: "Angelina Tango",
     hp: 4,
-    set: "ZHP",
+    set: "Za Hranicí Prérie",
     type: "Postava",
     text: "Ve svém tahu můžeš až jednou za tah prohodit 2 karty na stole.",
     image: "images/bang/cards/characters/angelina_tango.png"
@@ -39,7 +39,7 @@ const character_cards = [
     id: 5,
     name: "Anita Angel",
     hp: 4,
-    set: "ZHP",
+    set: "Za Hranicí Prérie",
     type: "Postava",
     text: "Poté co protihráč zraní jiného hráče, označ útočníka za Psance. Tvůj dostřel je proti Psancům nekonečný a při jejich zásahu kartou BANG! si dobereš kartu. Na konci tvého tahu jsou všichni Psanci ospravedlněni.",
     image: "images/bang/cards/characters/anita_angel.png"
@@ -48,7 +48,7 @@ const character_cards = [
     id: 6,
     name: "Carla Shaw",
     hp: 4,
-    set: "ZHP",
+    set: "Za Hranicí Prérie",
     type: "Postava",
     text: "Jednou za tah si můžeš líznout 2 karty, pokud zahraješ 3 karty za sebou, tak aby vytvořily vzrůstající nebo klesající číselnou řadu. (Na symbolu čísla nezáleží).",
     image: "images/bang/cards/characters/carla_shaw.png"
@@ -57,7 +57,7 @@ const character_cards = [
     id: 7,
     name: "Carmina Pale",
     hp: 4,
-    set: "ZHP",
+    set: "Za Hranicí Prérie",
     type: "Postava",
     text: "Začínáš hru s kartami Mustang a Appaloosa na stole. Karty tvých Koní jsou immuní vůči kartám Pravého poledne a kterýkoliv hráč jež tě o nějakého Koně připraví se stane cílem efektu BANG!",
     image: "images/bang/cards/characters/carmina_pale.png"
@@ -66,7 +66,7 @@ const character_cards = [
     id: 8,
     name: "Lady Constance",
     hp: 3,
-    set: "ZHP",
+    set: "Za Hranicí Prérie",
     type: "Postava",
     text: "Pokud bys měl/a být vyřazen/a, namísto opuštění hry si vyměň postavu za Polomrtvou Lady. Tvůj limit karet v ruce je vždy 4.",
     image: "images/bang/cards/characters/lady_constance.png"
@@ -75,7 +75,7 @@ const character_cards = [
     id: 9,
     name: "Lisa Elend",
     hp: 4,
-    set: "ZHP",
+    set: "Za Hranicí Prérie",
     type: "Postava",
     text: "Kdykoliv zraníš jednoho nebo více hráčů, vyléči si 1 zranění. (Namísto toho 2 zranění, pokud tvůj útok hráče vyřadil.)",
     image: "images/bang/cards/characters/lisa_elend.png"
